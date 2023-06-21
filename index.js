cd80183f4f1f858842bf5ad5bcce770f4a412393
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-06-21 12:34:13 -0400

    Added basic arithmetic functions

---
+++ index.js
@@ -0,0 +1,22 @@
+// Arithmetic functions
+const add = (x, y) => {
+    return x + y;
+}
+
+const subtract = (x, y) => {
+    return x - y;
+}
+
+const multiply = (x, y) => {
+    return x * y;
+}
+
+const divide = (x, y) => {
+    return x / y;
+}
+
+// Variables needed for calculations; will be user inputted
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
